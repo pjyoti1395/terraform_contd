4e127ea369b63981b15450d61a70976e6a72c5ef
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-09 20:53:30 +0530

    up

--- lifecycle/replace_triggered_by.tf
+++ lifecycle/replace_triggered_by.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "replace_triggered_demo" {
   ami           = "ami-02781fbdc79017564"
   instance_type = "t3.micro"
-  subnet_id     =  "subnet-051e2582de50c27d9"
+  subnet_id     = "subnet-0efdd9adaedbf45eb"  
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 
   lifecycle {
