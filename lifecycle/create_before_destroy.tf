b6c965443e470b60d946af04e2181a23d8d921ba
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-09 15:55:03 +0530

    Changed from t3.micro -> t3.small

--- lifecycle/create_before_destroy.tf
+++ lifecycle/create_before_destroy.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "demo" {
   ami           = "ami-02781fbdc79017564"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
 
   lifecycle {
     create_before_destroy = true
