4ffc6daccb8ef84ad41c0a106114512388220262
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-10 00:46:24 +0530

    added ssm with keys

--- secrets management/ssm.tf
+++ secrets management/ssm.tf
@@ -4,13 +4,13 @@ provider "aws" {
 
 # Fetch secret from SSM
 data "aws_ssm_parameter" "db_password" {
-  name           = "/myapp/db_password"
+  name           = "/myapp/db_password"  
   with_decryption = true
 }
 
 # Example: Use secret in EC2 user_data
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0abcd1234abcd1234"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   key_name      = "my-key-pair"
 
