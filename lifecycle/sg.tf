d1beae3016e6514b6b0ee159c2f2b7937f214afa
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-09 21:06:36 +0530

    added new ingress rule

--- lifecycle/sg.tf
+++ lifecycle/sg.tf
@@ -4,5 +4,13 @@ resource "aws_security_group" "my_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
+
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
