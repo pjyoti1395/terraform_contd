61ce5ae92ea1f31cbdbc8f57e8263ffd5db0c859
JyotiPanigrahi <panigrahij95@gmail.com>
2026-02-09 17:53:20 +0530

    up

--- lifecycle/ignore_changes.tf
+++ lifecycle/ignore_changes.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ignore_changes" {
   ami           = "ami-02781fbdc79017564"
   instance_type = "t3.micro"
-  subnet_id     =  "subnet_1111"
+  subnet_id     =  "subnet-0e5c006eebd9c3f91"
    tags = {
     Name = "ignore-changes-ec2"
     Env  = terraform.workspace
